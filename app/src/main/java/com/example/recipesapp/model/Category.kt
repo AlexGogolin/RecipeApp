cb31f27d1bf97e0a2b71679b3b56b44df48b68bc
AlexGogolin <agogolin89@mail.ru>
2026-06-26 13:32:00 +0300

    RA-06 fix

--- app/src/main/java/com/example/recipesapp/model/Category.kt
+++ app/src/main/java/com/example/recipesapp/model/Category.kt
@@ -3,5 +3,6 @@ package com.example.recipesapp.model
 data class Category(
     val id: Int,
     val title: String,
+    val description: String,
     val imageUrl: String,
 )
--- app/src/main/java/com/example/recipesapp/model/recipes.kt
+++ app/src/main/java/com/example/recipesapp/model/recipes.kt
@@ -2,10 +2,10 @@ package com.example.recipesapp.model
 
 object STUB{
     private val categories: List<Category> = listOf(
-        Category(1, "Бургеры", "burger.png"),
-        Category(2,"Десерты", "desert.png"),
-        Category(3,"Рыба", "fish.png"),
-        Category(4,"Пицца", "pizza.png"),
+        Category(1, "Бургеры", "Рецепты всех популярных видов бургеров", "burger.png"),
+        Category(2,"Десерты","Самые вкусные рецепты десертов специально для вас", "dessert.png"),
+        Category(3,"Рыба", "Печеная, жареная, сушеная, любая рыба на твой вкус", "fish.png"),
+        Category(4,"Пицца","Пицца на любой вкус и цвет. Лучшая подборка для тебя", "pizza.png"),
     )
     fun getCategories() = categories
 }
