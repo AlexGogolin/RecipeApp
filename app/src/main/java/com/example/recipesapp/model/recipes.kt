e9f029b5884dc0f09853ee842f2942a8af47bf3e
AlexGogolin <agogolin89@mail.ru>
2026-09-01 13:49:24 +0300

    RA-18 done

--- app/src/main/java/com/example/recipesapp/model/recipes.kt
+++ app/src/main/java/com/example/recipesapp/model/recipes.kt
@@ -47,4 +47,8 @@ object STUB{
     fun getRecipeById(recipeId: Int): Recipe?{
         return burgerRecipes.find {it.id == recipeId}
     }
+
+    fun getRecipesByIds(ids: Set<Int>): List<Recipe> {
+        return burgerRecipes.filter { it.id in ids }
+    }
 }
